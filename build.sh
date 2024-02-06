547757df112671b9101f1e08ac287fd95c82a2ac
vlasrado <vlasrado@google.com>
2024-02-06 20:52:58 +0000

    Adding and updating dockerfiles

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 #eval $(minikube docker-env)
-
+# 
 names=("go-client" "node-client" "java-client" "server" "cpp-client")
 
 openssl genrsa -out cert-config/ca-key.pem 2048
@@ -11,7 +11,7 @@ for name in "${names[@]}"; do
     docker build -f Dockerfiles/$name/Dockerfile -t $name:v1 .
     openssl genrsa -out cert-config/$name-key.pem 2048
     cp cert-config/cert.cnf /tmp/config.cnf
-    echo "DNS.3 = ${name}.default.svc" >> "/tmp/config.cnf"
+    echo "DNS.3 = ${name}.default.svc.cluster.local" >> "/tmp/config.cnf"
     openssl req -new -key cert-config/$name-key.pem -out cert-config/$name.csr -config /tmp/config.cnf
     openssl x509 -req -in cert-config/$name.csr -CA cert-config/ca.pem -CAkey cert-config/ca-key.pem -CAcreateserial -out cert-config/$name-cert.pem -days 365 -sha256 -extensions v3_req -extfile /tmp/config.cnf
 done
