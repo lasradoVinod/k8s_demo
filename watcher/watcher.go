a2efb4c0e5452ab44e21d19f44edc56b2a6a7bd1
vlasrado <vlasrado@google.com>
2024-02-22 21:44:11 +0000

    Started testing

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 
 	//	"os"
-
 	"strings"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -15,41 +18,121 @@ import (
 )
 
 type PodInfo struct {
-  nodeName string
-  containerId string
+	nodeName    string
+	containerId []string
 }
-type Info struct {
-   // map of node name to lightfoot pod ip on that node
-   nodes  map[string] string
-   // map of pod names to container_id
-   // These are all pods which have lightfoot:enable
-   container   map[string] PodInfo
+
+type ControllerInfo struct {
+	// map of node name to lightfoot pod ip on that node
+	nodes map[string]string
+	// map of pod names to container_id
+	// These are all pods which have lightfoot:enable
+	container map[string]PodInfo
+	// List of containers lightfoot is not yet updated about
+	pending map[string]bool
+	// Channel to trigger new pending
+	wake chan struct{}
+	// lock for adding to pending state
+	mu sync.Mutex
 }
 
 type PodEvent int
+
 // Enum for Pod updates
 const (
-	Add PodEvent = iota
-	Update = iota
-	Delete = iota
+	Add    PodEvent = iota
+	Update          = iota
+	Delete          = iota
 )
 
-func printPodInfo(pod *v1.Pod) {
-	fmt.Println(pod.Spec.NodeName, pod.Status.PodIP, pod.ObjectMeta.GetName(), pod.ObjectMeta.GetLabels())
-	for _, containerStatus := range pod.Status.ContainerStatuses {
-                containerID := containerStatus.ContainerID
-                if strings.HasPrefix(containerID, "cri-o://") {
-                        containerID = containerID[8:] 
-                }
-                fmt.Printf("Container: %s, ID: %s\n", containerStatus.Name, containerID)
-        }
+var controllerInfo ControllerInfo
 
+func (c *ControllerInfo) SendUpdate(name string) bool {
+	var ip string
+	var ok bool
+	if ip, ok = c.nodes[c.container[name].nodeName]; !ok {
+		return false
+	}
+	var buffer bytes.Buffer
+	for _, str := range c.container[name].containerId {
+		buffer.WriteString(str)
+		buffer.WriteString("\n") // Add a newline if you want separation
+	}
+	req, err := http.NewRequest("POST", "https://"+ip+":12000/crio-id", bytes.NewReader(buffer.Bytes()))
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return false
+	}
+	req.Header.Set("Content-Type", "text/plain")
+	// Create an HTTP client and perform the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode < 0 || resp.StatusCode > 300 {
+		fmt.Println("Request failed with status:", buffer, resp.Status)
+		return false
+	}
+	return true
 }
 
-func handlePodEvent(e PodEvent, pod *v1.Pod){
-   switch e {
+func (c *ControllerInfo) ContactLightroom() {
+	timer := time.NewTimer(10 * time.Second)
 
-   }
+	for {
+		select {
+		case <-timer.C:
+		case <-c.wake:
+			c.mu.Lock()
+			for k, _ := range c.pending {
+				status := c.SendUpdate(k)
+				if status {
+					delete(c.pending, k)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}
+}
+
+func handlePodEvent(e PodEvent, pod *v1.Pod) {
+	switch e {
+	case Add:
+		done := false
+		// Add the ip to the lightfoot container corresponding to nodename
+		name := pod.ObjectMeta.GetName()
+		if strings.HasPrefix(name, "lightfoot-daemon") {
+			controllerInfo.nodes[pod.Spec.NodeName] = pod.Status.PodIP
+			break
+		}
+		for k, v := range pod.ObjectMeta.GetLabels() {
+			if k == "lightfoot" && v == "enable" {
+				done = true
+				break
+			}
+		}
+		if !done {
+			break
+		}
+		var p PodInfo
+		p.nodeName = pod.Spec.NodeName
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			p.containerId = append(p.containerId, strings.TrimPrefix(containerStatus.ContainerID, "cri-o://"))
+		}
+		controllerInfo.container[name] = p
+		controllerInfo.mu.Lock()
+		controllerInfo.pending[name] = false
+		controllerInfo.mu.Unlock()
+		controllerInfo.wake <- struct{}{}
+	case Update:
+	case Delete:
+		// Handle these cases later revisions
+	default:
+		break
+	}
 }
 
 func main() {
@@ -63,6 +146,8 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	controllerInfo.wake = make(chan struct{})
 
 	factory := informers.NewSharedInformerFactory(clientset, 2*time.Second)
 	podInformer := factory.Core().V1().Pods()
@@ -87,4 +172,3 @@ func main() {
 	// Keep the program running.
 	select {}
 }
-
