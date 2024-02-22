9baa84d7e335a88fe7b0645e9427fd4c744dfdd1
vlasrado <vlasrado@google.com>
2024-02-22 01:04:50 +0000

    Initial implemtation

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -6,12 +6,51 @@ import (
 
 	//	"os"
 
+	"strings"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 )
+
+type PodInfo struct {
+  nodeName string
+  containerId string
+}
+type Info struct {
+   // map of node name to lightfoot pod ip on that node
+   nodes  map[string] string
+   // map of pod names to container_id
+   // These are all pods which have lightfoot:enable
+   container   map[string] PodInfo
+}
+
+type PodEvent int
+// Enum for Pod updates
+const (
+	Add PodEvent = iota
+	Update = iota
+	Delete = iota
+)
+
+func printPodInfo(pod *v1.Pod) {
+	fmt.Println(pod.Spec.NodeName, pod.Status.PodIP, pod.ObjectMeta.GetName(), pod.ObjectMeta.GetLabels())
+	for _, containerStatus := range pod.Status.ContainerStatuses {
+                containerID := containerStatus.ContainerID
+                if strings.HasPrefix(containerID, "cri-o://") {
+                        containerID = containerID[8:] 
+                }
+                fmt.Printf("Container: %s, ID: %s\n", containerStatus.Name, containerID)
+        }
+
+}
+
+func handlePodEvent(e PodEvent, pod *v1.Pod){
+   switch e {
+
+   }
+}
 
 func main() {
 	// Create a Kubernetes client using the provided kubeconfig.
@@ -31,15 +70,14 @@ func main() {
 	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			pod := obj.(*v1.Pod)
-			fmt.Println("Pod Added", pod.Name, pod.Status.Phase)
+			handlePodEvent(Add, pod)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			pod := oldObj.(*v1.Pod)
-			fmt.Println("Pod Updated", pod.Name, pod.Status.Phase)
+			handlePodEvent(Update, pod)
 		},
 		DeleteFunc: func(obj interface{}) {
-			pod := obj.(*v1.Pod)
-			fmt.Println("Pod Deleted", pod.Name, pod.Status.Phase)
+			//Do nothing we don't care about deletes for now
 		},
 	})
 	stopCh := make(chan struct{})
